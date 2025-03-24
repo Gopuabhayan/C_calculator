08a6a97e9714ede95cec975f77b4d28e7780f726
Gopuabhayan <gopuabhayan5@gmail.com>
2025-03-24 13:49:47 +0530

    created main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{   
+    {
+        int l,w,area;
+        printf("enter a lenght\n");
+        scanf("%d",&l);
+        printf("enter a width\n");
+        scanf("%d",&w);
+        area=l*w;
+        printf("area=%d\n",area);
+    }
+         
+}
